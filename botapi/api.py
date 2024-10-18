22ca04e64e188de4edde1eb199dc6e92e3d6664e
stefano <69367859+stefanodvx@users.noreply.github.com>
2024-10-18 13:12:15 +0200

    Update api.py

--- botapi/api.py
+++ botapi/api.py
@@ -16,11 +16,13 @@ class BotAPI(BaseModel, Methods):
     token: str
     api_url: str = "https://api.telegram.org"
     parse_mode: str = "HTML"
-    session: httpx.AsyncClient = httpx.AsyncClient(timeout=120)
+    session: httpx.AsyncClient = Field(
+        default=httpx.AsyncClient(timeout=120))
     sudoers: List[int] = Field(default_factory=list)
 
     class Config:
         arbitrary_types_allowed = True
+
 
     def _compose_api_url(self, method: str) -> str:
         return f"{self.api_url}/bot{self.token}/{method}"
