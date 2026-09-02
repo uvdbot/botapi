a7173404d7000621c3ae8206850d0c10067560d3
stefanodvx <69367859+stefanodvx@users.noreply.github.com>
2026-09-02 03:58:27 +0000

    Updated to 2026.09.02

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="botapi",
-    version="2026.09.01",
+    version="2026.09.02",
     install_requires=[
         "httpx",
         "pydantic",
